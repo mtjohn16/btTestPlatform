d157a508a9a29260c732746035022d099f771f9d
mtjohn16 <mtjohn16@asu.edu>
2022-10-17 11:08:09 -0700

    Update applePay.php

--- applePay.php
+++ applePay.php
@@ -49,88 +49,90 @@
 				console.error('This device is not capable of making Apple Pay payments');
 			}
 
-			braintree.client.create({
-				authorization: '<?php echo $clientToken;?>'
-			}, function (clientErr, clientInstance) {
-				if (clientErr) {
-					console.error('Error creating client:', clientErr);
-					return;
-				}
+			applePayButton.addEventListener('click', function() {
 
-				braintree.applePay.create({
-					client: clientInstance
-				}, function (applePayErr, applePayInstance) {
-					if (applePayErr) {
-						console.error('Error creating applePayInstance:', applePayErr);
+
+
+				braintree.client.create({
+					authorization: '<?php echo $clientToken;?>'
+				}, function (clientErr, clientInstance) {
+					if (clientErr) {
+						console.error('Error creating client:', clientErr);
 						return;
 					}
 
-					// Set up your Apple Pay button here
-
-					var paymentRequest = applePayInstance.createPaymentRequest({
-						total: {
-							label: 'My Store',
-							amount: '19.99'
-						},
-
-						// We recommend collecting billing address information, at minimum
-						// billing postal code, and passing that billing postal code with
-						// all Apple Pay transactions as a best practice.
-						requiredBillingContactFields: ["postalAddress"]
-					});
-					console.log(paymentRequest.countryCode);
-					console.log(paymentRequest.currencyCode);
-					console.log(paymentRequest.merchantCapabilities);
-					console.log(paymentRequest.supportedNetworks);
-
-					var session = new ApplePaySession(3, paymentRequest);
-
-					session.onvalidatemerchant = function (event) {
-						applePayInstance.performValidation({
-							validationURL: event.validationURL,
-							displayName: 'My Store'
-						}, function (err, merchantSession) {
-							if (err) {
-							// You should show an error to the user, e.g. 'Apple Pay failed to load.'
+					braintree.applePay.create({
+						client: clientInstance
+					}, function (applePayErr, applePayInstance) {
+						if (applePayErr) {
+							console.error('Error creating applePayInstance:', applePayErr);
 							return;
-							}
-							session.completeMerchantValidation(merchantSession);
+						}
+
+						// Set up your Apple Pay button here
+
+						var paymentRequest = applePayInstance.createPaymentRequest({
+							total: {
+								label: 'My Store',
+								amount: '19.99'
+							},
+
+							// We recommend collecting billing address information, at minimum
+							// billing postal code, and passing that billing postal code with
+							// all Apple Pay transactions as a best practice.
+							requiredBillingContactFields: ["postalAddress"]
 						});
-					};
+						console.log(paymentRequest.countryCode);
+						console.log(paymentRequest.currencyCode);
+						console.log(paymentRequest.merchantCapabilities);
+						console.log(paymentRequest.supportedNetworks);
 
-					session.onpaymentauthorized = function (event) {
-						console.log('Your shipping address is:', event.payment.shippingContact);
+						var session = new ApplePaySession(3, paymentRequest);
 
-						applePayInstance.tokenize({
-							token: event.payment.token
-						}, function (tokenizeErr, payload) {
-							if (tokenizeErr) {
-								console.error('Error tokenizing Apple Pay:', tokenizeErr);
-								session.completePayment(ApplePaySession.STATUS_FAILURE);
+						session.onvalidatemerchant = function (event) {
+							applePayInstance.performValidation({
+								validationURL: event.validationURL,
+								displayName: 'My Store'
+							}, function (err, merchantSession) {
+								if (err) {
+								// You should show an error to the user, e.g. 'Apple Pay failed to load.'
 								return;
-							}
+								}
+								session.completeMerchantValidation(merchantSession);
+							});
+						};
 
-							// Send payload.nonce to your server.
-							console.log('nonce:', payload.nonce);
+						session.onpaymentauthorized = function (event) {
+							console.log('Your shipping address is:', event.payment.shippingContact);
 
-							// If requested, address information is accessible in event.payment
-							// and may also be sent to your server.
-							console.log('billingPostalCode:', event.payment.billingContact.postalCode);
+							applePayInstance.tokenize({
+								token: event.payment.token
+							}, function (tokenizeErr, payload) {
+								if (tokenizeErr) {
+									console.error('Error tokenizing Apple Pay:', tokenizeErr);
+									session.completePayment(ApplePaySession.STATUS_FAILURE);
+									return;
+								}
 
-							// After you have transacted with the payload.nonce,
-							// call `completePayment` to dismiss the Apple Pay sheet.
-							session.completePayment(ApplePaySession.STATUS_SUCCESS);
-						});
-					};
+								// Send payload.nonce to your server.
+								console.log('nonce:', payload.nonce);
 
-					applePayButton.addEventListener('click', function() {
-						//start the AP session, and do any other stuff when AP button is clicked
+								// If requested, address information is accessible in event.payment
+								// and may also be sent to your server.
+								console.log('billingPostalCode:', event.payment.billingContact.postalCode);
+
+								// After you have transacted with the payload.nonce,
+								// call `completePayment` to dismiss the Apple Pay sheet.
+								session.completePayment(ApplePaySession.STATUS_SUCCESS);
+							});
+						};
+
 						session.begin();
+
+		
 					});
-	
 				});
 			});
-
 		</script>
 	</body>
 </html>
