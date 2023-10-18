e8203caf53e87c0a95db944611238bcceb72ef99
mtjohn16 <mtjohn16@asu.edu>
2023-10-18 11:28:41 -0700

    Update venmo.php
    jquery correction maybe

--- venmo.php
+++ venmo.php
@@ -40,6 +40,13 @@
 			</p>
 		</div>
 		<script>
+		     jQuery.noConflict();
+		     // Use jQuery via jQuery() instead of via $()
+		     jQuery(document).ready(function(){
+		       jQuery("div").hide();
+		     });  
+		</script>
+		<script>
 
 			var venmoButton = document.getElementById('venmo-button');
 
@@ -143,10 +150,10 @@
 			// Send the payment method nonce to your server, e.g. by injecting
 			// it into your form as a hidden input.
 			console.log('Got a payment method nonce:', payload.nonce);
-			$("#nonce").html(payload.nonce);
+			jQuery("#nonce").html(payload.nonce);
 			// Display the Venmo username in your checkout UI.
 			console.log('Venmo user:', payload.details.username);
-			$("#username").html(payload.details.username);
+			jQuery("#username").html(payload.details.username);
 			}
 		</script>
 					
