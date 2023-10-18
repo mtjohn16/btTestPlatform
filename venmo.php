7c494d46353079154c5aaf37f9810beb7dea316a
mtjohn16 <mtjohn16@asu.edu>
2023-10-18 11:31:47 -0700

    Update venmo.php

--- venmo.php
+++ venmo.php
@@ -14,7 +14,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<script scr="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script scr="https://code.jquery.com/jquery-3.7.0.js"></script>
 		<script src="https://js.braintreegateway.com/web/3.97.2/js/client.min.js"></script>
 		<script src="https://js.braintreegateway.com/web/3.97.2/js/venmo.min.js"></script>
 		<script src="https://js.braintreegateway.com/web/3.97.2/js/data-collector.min.js"></script>
